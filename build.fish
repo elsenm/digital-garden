5d5340c67ba875b9a204061e9a65ea47becff922
Marvin Elsen <development@marvin-elsen.com>
2020-09-30 22:24:09 +0200

    Convert build script to fish

--- build.fish
+++ build.fish
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/usr/bin/fish
 git add .
-git commit -S -m "$1"
+git commit -S -m $argv[1] 
 git push
 mkdocs build
 sshpass -f ~/.ssh/wiki scp -r ./site/* "$WIKI_USER"@"$WIKI_SERVER":/
