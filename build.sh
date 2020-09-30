447d25b14d7d53373e17a92ab92a2457ab6da701
Marvin Elsen <development@marvin-elsen.com>
2020-09-30 22:16:28 +0200

    Add exclude plugin to ignore main.md and add build.sh to git

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ git add .
 git commit -S -m "$1"
 git push
 mkdocs build
-sshpass -f ~/.ssh/wiki scp -r ./site/* $WIKI_USER@$WIKI_SERVER:/
+sshpass -f ~/.ssh/wiki scp -r ./site/* "$WIKI_USER"@"$WIKI_SERVER":/
 rm -r site
