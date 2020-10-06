130c1d88050237c4f7905eb1d4f818ce367f9e5f
Marvin Elsen <development@marvin-elsen.com>
2020-10-06 20:30:56 +0200

    Optimize publish script

--- build.fish
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/fish
-git add .
-git commit -S -m $argv[1] 
-git push
-mkdocs build
-sshpass -f ~/.ssh/wiki scp -r ./site/* "$WIKI_USER"@"$WIKI_SERVER":/
-rm -r site
---
+++ publish.fish
@@ -0,0 +1,7 @@
+#!/usr/bin/fish
+git add .
+git commit -S -m $argv[1] 
+git push
+mkdocs build
+sshpass -f ~/.ssh/wiki rsync -aq --no-perms --omit-dir-times --delete site/ "$WIKI_USER"@"$WIKI_SERVER":/mnt/web009/d3/76/57512676/htdocs/
+rm -r site
