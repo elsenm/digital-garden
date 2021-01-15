147bf5007645c0787f5f6802e81d40ef6fd54abc
Marvin Elsen <contact@marvin-elsen.com>
2021-01-16 00:14:30 +0100

    Remove Keybase proof

--- publish.fish
+++ publish.fish
@@ -1,6 +1,6 @@
 #!/usr/bin/fish
 git add .
-git commit -S -m $argv[1] 
+git commit --gpg-sign="$WIKI_GPG_KEY" -m $argv[1] 
 git push
 mkdocs build
 sshpass -f ~/.ssh/wiki rsync -aq --no-perms --omit-dir-times site/ "$WIKI_USER"@"$WIKI_SERVER":/mnt/web009/d3/76/57512676/htdocs/
