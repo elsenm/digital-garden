d7b30b5ba309b29797ed251a65ad49ccd2ab1901
Marvin Elsen <development@marvin-elsen.com>
2021-01-16 00:35:24 +0100

    Update publish script

--- publish.fish
+++ publish.fish
@@ -1,6 +1,8 @@
 #!/usr/bin/fish
+. .venv/bin/activate.fish
 git add .
 git commit --gpg-sign="$WIKI_GPG_KEY" -m $argv[1] 
 git push
 mkdocs build
 sshpass -f ~/.ssh/wiki rsync -aq --no-perms --omit-dir-times site/ "$WIKI_USER"@"$WIKI_SERVER":/mnt/web009/d3/76/57512676/htdocs/
+deactivate
