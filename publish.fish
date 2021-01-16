2747e00139eb0604a18550454f42b871b753f37a
Marvin Elsen <development@marvin-elsen.com>
2021-01-16 01:35:48 +0100

    Add quiet mode to all commands in publish script and echo messages

--- publish.fish
+++ publish.fish
@@ -1,10 +1,17 @@
 #!/usr/bin/fish
-jpegoptim -ptsv (ls -d docs/assets/images/* | string match -er '.jpg|.jpeg|.JPG|.JPEG')
-optipng (ls -d docs/assets/images/* | string match -er '.png|.PNG')
+echo 'Optimizing JPEG images...'
+jpegoptim -ptsq (ls -d docs/assets/images/* | string match -er '.jpg|.jpeg|.JPG|.JPEG')
+echo 'Optimizing PNG images...'
+optipng -strip -preserve -o7 -quiet (ls -d docs/assets/images/* | string match -er '.png|.PNG')
+echo 'Adding changes to git...'
 git add .
-git commit --gpg-sign="$WIKI_GPG_KEY" -m $argv[1] 
-git push
+echo 'Committing changes to git...'
+git commit --quiet --gpg-sign="$WIKI_GPG_KEY" -m $argv[1] 
+echo 'Pushing changes to GitHub...'
+git push --quiet
+echo 'Building website...'
 . .venv/bin/activate.fish
-mkdocs build
+mkdocs build --quiet
 deactivate
+echo 'Uploading website...'
 sshpass -f ~/.ssh/wiki rsync -aq --no-perms --omit-dir-times site/ "$WIKI_USER"@"$WIKI_SERVER":/mnt/web009/d3/76/57512676/htdocs/
