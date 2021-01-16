70ea5192ba57e62ffe352585334203a46af5e310
Marvin Elsen <development@marvin-elsen.com>
2021-01-16 01:26:35 +0100

    Add image compression to publish script

--- publish.fish
+++ publish.fish
@@ -1,8 +1,10 @@
 #!/usr/bin/fish
-. .venv/bin/activate.fish
+jpegoptim -ptsv (ls -d docs/assets/images/* | string match -er '.jpg|.jpeg|.JPG|.JPEG')
+optipng (ls -d docs/assets/images/* | string match -er '.png|.PNG')
 git add .
 git commit --gpg-sign="$WIKI_GPG_KEY" -m $argv[1] 
 git push
+. .venv/bin/activate.fish
 mkdocs build
-sshpass -f ~/.ssh/wiki rsync -aq --no-perms --omit-dir-times site/ "$WIKI_USER"@"$WIKI_SERVER":/mnt/web009/d3/76/57512676/htdocs/
 deactivate
+sshpass -f ~/.ssh/wiki rsync -aq --no-perms --omit-dir-times site/ "$WIKI_USER"@"$WIKI_SERVER":/mnt/web009/d3/76/57512676/htdocs/
